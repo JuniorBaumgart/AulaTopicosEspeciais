18386b4bd1512fb8ef1670748fbeddba5e73d6bb
Junior de Souza <juni019baungart@hotmail.com>
2026-08-11 14:02:28 -0300

    Ajuste de Ordem de Execução no Middleware

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import { rotas } from "./routes/router";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get(("/health"),(request,response)=>{
     console.log('verificando status');
     response.status(200).json({message: "Sucesso"})
 });
 
 app.use(rotas);
-
-app.use(express.json());
 
 app.listen(port, ()=>{
     console.log(`Port aquired : http://localhost:${port}`);
